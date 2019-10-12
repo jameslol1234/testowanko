8048f9a0c3e941abda37290e645bb53dedb7e9bd
jameslol1234 <krecik20001@wp.pl>
2019-10-12 02:25:53 +0200

    FULL PAKIET

--- testowanko/Program.cs
+++ testowanko/Program.cs
@@ -3,12 +3,118 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace Projekt
 {
     class Program
     {
+        static void DLFile()
+        {
+            Console.WriteLine("1. Download file from internet.");
+            string remoteUri = "https://s3.zylowski.net/public/input/";
+            string fileName = "3.txt";
+            WebClient myWebClient = new WebClient();
+            string myStringWebResource = remoteUri + fileName;
+            Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", fileName, myStringWebResource);
+            myWebClient.DownloadFile(myStringWebResource, fileName);
+            Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", fileName, myStringWebResource);
+        }
+        static int CountLetters()
+        {
+            Console.WriteLine("2. Count number of letters in the file.");
+            string FileText;
+            try
+            {
+                FileText = File.ReadAllText("3.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Could not find file 3.txt");
+                return 1;
+            }
 
+            int LettersQty = 0;
+            foreach (char c in FileText)
+            {
+                if (!char.IsWhiteSpace(c) && char.IsLetter(c))
+                {
+                    LettersQty++;
+                }
+            }
+            Console.WriteLine("Count of letters in file: " + LettersQty);
+            return 0;
+        }
+        static int CountWords()
+        {
+            Console.WriteLine("3. Count number of words in the file.");
+            string FileText;
+            try
+            {
+                FileText = File.ReadAllText("3.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Could not find file 3.txt");
+                return 1;
+            }
+            string[] Words = FileText.Split(' ');
+            int WordsQty = 0;
+            foreach (string word in Words)
+            {
+                WordsQty++;
+            }
+            Console.WriteLine("Number of words is: " + WordsQty);
+            return 0;
+        }
+        static int CountPuncMarks()
+        {
+            Console.WriteLine("4. Count number of punctuation marks in the file.");
+            string FileText;
+            try
+            {
+                FileText = File.ReadAllText("3.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Could not find file 3.txt");
+                return 1;
+            }
+
+            int PuncMarksQty = 0;
+            foreach (char c in FileText)
+            {
+                if (char.IsPunctuation(c))
+                {
+                    PuncMarksQty++;
+                }
+            }
+            Console.WriteLine("Count of punctuation marks in file: " + PuncMarksQty);
+            return 0;
+        }
+        static int CountSentence()
+        {
+            Console.WriteLine("5. Count number of sentences in the file.");
+            string FileText;
+            try
+            {
+                FileText = File.ReadAllText("3.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Could not find file 3.txt");
+                return 1;
+            }
+            string[] Words = FileText.Split('.', '?', ';', '!');
+            int SentenceQty = 0;
+            foreach (string word in Words)
+            {
+                SentenceQty++;
+            }
+            Console.WriteLine("Number of sentences is: " + SentenceQty);
+            return 0;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("TEXT ANALYZER");
@@ -16,9 +122,6 @@ namespace Projekt
             {
                 Console.WriteLine("\nMENU: \n1. Download file from internet.\n2. Count number of letters in the file.\n3. Count number of words in the file.\n4. Count number of punctuation marks in the file.\n5. Count number of sentences in the file.\n6. Report about usage of letters (A-Z).\n7. Save statistics from points 2-5 to the file(statystki.txt)\n8. Exit and close application.\n");
                 Console.Write("Choose option to execute: ");
-                Console.WriteLine("JD");
-                Console.WriteLine("JD");
-                Console.WriteLine("JD");
                 int MenuOpt;
                 if (!int.TryParse(Console.ReadLine(), out MenuOpt))
                 {
@@ -27,42 +130,92 @@ namespace Projekt
                 }
                 if (MenuOpt == 1)
                 {
-                    Console.WriteLine("1. Download file from internet.");
-                    Console.WriteLine("add some code here...");
-                    continue;
+                    DLFile();
                 }
                 else if (MenuOpt == 2)
                 {
-                    Console.WriteLine("2. Count number of letters in the file.");
-                    Console.WriteLine("add some code here...");
+                    if (CountLetters() == 1) continue;
                 }
                 else if (MenuOpt == 3)
                 {
-                    Console.WriteLine("3. Count number of words in the file.");
-                    Console.WriteLine("add some code here...");
+                    if (CountWords() == 1) continue;
                 }
                 else if (MenuOpt == 4)
                 {
-                    Console.WriteLine("4. Count number of punctuation marks in the file.");
-                    Console.WriteLine("add some code here...");
+                    if (CountPuncMarks() == 1) continue;
                 }
                 else if (MenuOpt == 5)
                 {
-                    Console.WriteLine("5. Count number of sentences in the file.");
-                    Console.WriteLine("add some code here...");
+                    if (CountSentence() == 1) continue;
                 }
                 else if (MenuOpt == 6)
                 {
                     Console.WriteLine("6. Report about usage of letters (A-Z).");
-                    Console.WriteLine("add some code here...");
+                    int[] c = new int[(int)char.MaxValue];
+                    string FileText;
+                    try
+                    {
+                        FileText = File.ReadAllText("3.txt");
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        Console.WriteLine("Could not find file 3.txt");
+                        continue;
+                    }
+                    foreach (char t in FileText)
+                    {
+                        c[(int)t]++;
+                    }
+                    for (int i = 0; i < (int)char.MaxValue; i++)
+                    {
+                        if (c[i] > 0 &&
+                            char.IsLetter((char)i))
+                        {
+                            Console.WriteLine("{0} : {1}",
+                                (char)i,
+                                c[i]);
+                        }
+                    }
                 }
                 else if (MenuOpt == 7)
                 {
                     Console.WriteLine("7. Save statistics from points 2-5 to the file(statystki.txt)");
-                    Console.WriteLine("add some code here...");
+                    FileStream ostrm;
+                    StreamWriter writer;
+                    TextWriter oldOut = Console.Out;
+                    try
+                    {
+                        ostrm = new FileStream("./statystyki.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                        writer = new StreamWriter(ostrm);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Cannot open statystyki.txt for writing");
+                        Console.WriteLine(e.Message);
+                        return;
+                    }
+                    Console.SetOut(writer);
+                    CountLetters();
+                    CountWords();
+                    CountPuncMarks();
+                    CountSentence();
+                    Console.SetOut(oldOut);
+                    writer.Close();
+                    ostrm.Close();
+                    Console.WriteLine("Saving to file done!");
                 }
                 else if (MenuOpt == 8)
                 {
+                    if (File.Exists("3.txt"))
+                    {
+                        File.Delete("3.txt");
+                        Console.WriteLine("Deleted file: 3.txt");
+                    }
+                    if (File.Exists("statystyki.txt"))
+                    {
+                        File.Delete("statystyki.txt");
+                        Console.WriteLine("Deleted file: statystyki.txt");
+                    }
                     Console.WriteLine("Exiting the program...");
                     break;
                 }
